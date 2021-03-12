edc81c46b1f125c549547267099e286eefbe79eb
James Crisafulli <james2048@gmail.com>
2021-03-12 12:18:58 +0000

    WSL compile

--- JACFFmpegLibZig/build.zig
+++ JACFFmpegLibZig/build.zig
@@ -2,22 +2,29 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("JACFFmpegLibZig", "src/main.zig");
+    const lib = b.addStaticLibrary("JACFFmpegLibZig", "src/inputurl.zig");
+    lib.linkLibC();
     lib.linkSystemLibrary("c");
     lib.linkSystemLibrary("avformat");
     lib.linkSystemLibrary("avcodec");
-    lib.addIncludeDir("FFmpegStaticLibs/include");
-    lib.addLibPath("FFmpegStaticLibs/lib");
+    lib.linkSystemLibrary("avutil");
+    lib.addIncludeDir("/usr/include/x86_64-linux-gnu/");
+    lib.addLibPath("/usr/lib/x86_64-linux-gnu/");
+    //lib.addIncludeDir("../FFmpegStaticLibs/include");
+    //lib.addLibPath("../FFmpegStaticLibs/lib");
     lib.setBuildMode(mode);
     lib.install();
 
-    var main_tests = b.addTest("src/main.zig");
+    var main_tests = b.addTest("src/inputurl.zig");
     main_tests.setBuildMode(mode);
     main_tests.linkSystemLibrary("c");
     main_tests.linkSystemLibrary("avformat");
     main_tests.linkSystemLibrary("avcodec");
-    main_tests.addIncludeDir("FFmpegStaticLibs/include");
-    main_tests.addLibPath("FFmpegStaticLibs/lib");
+    main_tests.linkSystemLibrary("avutil");
+    main_tests.addIncludeDir("/usr/include/x86_64-linux-gnu/");
+    main_tests.addLibPath("/usr/lib/x86_64-linux-gnu/");
+    //main_tests.addIncludeDir("../FFmpegStaticLibs/include");
+    //main_tests.addLibPath("../FFmpegStaticLibs/lib");
 
 
     const test_step = b.step("test", "Run library tests");
--- JACFFmpegLibZig/src/inputurl.zig
+++ JACFFmpegLibZig/src/inputurl.zig
@@ -1,29 +1,46 @@
 const std = @import("std");
 const testing = std.testing;
 
-const InputURLError = error {
+const c = @cImport({
+    @cInclude("libavutil/avutil.h");
+    @cInclude("libavformat/avformat.h");
+    @cInclude("libavcodec/avcodec.h");
+});
+
+const InputUrlError = error {
     DemuxerError,
     CodecError
 };
 
-const InputURL = struct {
+const InputUrl = struct {
     const Self = @This();
     
     url: []const u8,
+    formatCtx: [*c]c.struct_AVFormatContext = null,
+    codecCtx: [*c]c.struct_AVCodecContext = null,
+    videoStreamIndex: ?i32 = null,
+    bestVideoStream: [*c]c.struct_AVStream = null,
+    bestAudioStream: [*c]c.struct_AVStream = null,
 
-    fn init(self: Self) !InputURL {
-        return self;
+    fn init(self: Self) !void {
+        var ret : c_int = c.avformat_open_input(null, @ptrCast([*c]const u8, &self.url), null, null);
+
+        if (ret != 0) {
+            return error.DemuxerError;
+        }
     }
 
     fn deinit(self: Self) void {
-
+        // close contexts
     }
 };
 
-test "InputURL" {
-    const input = InputURL {
+test "InputUrl" {
+    var input = InputUrl {
         .url = "wow"
     };
+
+    try input.init();
 
     defer input.deinit();
 
